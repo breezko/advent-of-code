1fec5bfebc1e1f654d26c5951e520eb217a5adc5
Breezko <hendrik.heim@gmx.de>
2020-12-02 19:05:14 +0100

    cleanup

--- 2/solution.py
+++ 2/solution.py
@@ -7,6 +7,7 @@ import random
 import sys
 import timeit
 
+
 def main(lines: list) -> tuple:
     """https://adventofcode.com/2020/day/2
     r1 = Part 1 result
@@ -18,16 +19,17 @@ def main(lines: list) -> tuple:
     Returns:
         tuple: (part1,part2)
     """
-    r1,r2 = 0, 0
+    r1, r2 = 0, 0
     for line in lines:
-        a,b,c = line.split()
+        a, b, c = line.split()
         b = b[0]
-        lo, hi = map(int,a.split("-"))
+        lo, hi = map(int, a.split("-"))
         r1 += lo <= c.count(b) <= hi
         r2 += sum(c[x - 1] == b for x in (lo, hi)) == 1
     print(r1)
     print(r2)
-    return (r1,r2)
+    return (r1, r2)
+
 
 if __name__ == "__main__":
     fin = open("data.in", "r")
@@ -35,4 +37,3 @@ if __name__ == "__main__":
     main(lines)
     t = timeit.Timer(stmt="solution.main(lines)", setup="import solution")
     print("TASK: ", t.timer())
-    
