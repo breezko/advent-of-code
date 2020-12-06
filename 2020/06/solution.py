c99ef5bceacde3416d42dd3fe11e1e6c25ee8741
Breezko <hendrik.heim@gmx.de>
2020-12-06 12:18:44 +0100

    refactoring 06

--- 2020/06/solution.py
+++ 2020/06/solution.py
@@ -13,7 +13,7 @@ def main(lines: list) -> None:
     """https://adventofcode.com/2020/day/6"""
     ans1, ans2 = 0, 0
     group = set()
-    person = []
+    persons = []
     char_dict = {}
     ans2 = 0
     for line in lines:
@@ -21,21 +21,18 @@ def main(lines: list) -> None:
         if not line or line == lines[-1]:
             ans1 += len(group)
             group = set()
-            for p in person:
+            for p in persons:
                 for op in p:
-                    if op in char_dict:
-                        char_dict[op] += 1
-                    else:
-                        char_dict[op] = 1
+                    char_dict[op] = char_dict[op] + 1 if op in char_dict else 1
 
             for k, _ in char_dict.items():
-                if char_dict[k] == len(person):
+                if char_dict[k] == len(persons):
                     duplicates += 1
             ans2 += duplicates
-            person = []
+            persons = []
             char_dict = {}
             continue
-        person.append([char for char in line])
+        persons.append([char for char in line])
         for char in line:
             group.add(char)
 
