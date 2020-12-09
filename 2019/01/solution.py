f8cf03e378b613416a21a3da02d0f9e7c2f5794e
Breezko <hendrik.heim@gmx.de>
2020-12-09 16:05:15 +0100

    Day 01/2019

---
+++ 2019/01/solution.py
@@ -0,0 +1,27 @@
+from itertools import combinations
+import math
+
+
+def main(puzzle: list) -> int:
+    """https://adventofcode.com/2020/day/1"""
+
+    res = res2 = []
+    for line in puzzle:
+        res.append(math.floor(line / 3 - 2))
+
+    for item in res:
+        amt = item
+        module = []
+        while amt >= 0:
+            module.append(amt)
+            amt = math.floor(amt / 3 - 2)
+        res2.append(sum(module))
+
+    print(sum(res))
+    print(sum(res2))
+
+
+if __name__ == "__main__":
+    fin = open("data.in", "r")
+    lines = [int(line.strip()) for line in fin.readlines()]
+    main(lines)
