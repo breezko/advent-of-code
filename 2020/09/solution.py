f767f3f0e28528cb1e29270d39b01a5ec0bc30ff
Breezko <hendrik.heim@gmx.de>
2020-12-09 12:12:01 +0100

    Day 09

---
+++ 2020/09/solution.py
@@ -0,0 +1,52 @@
+from collections import *
+from functools import lru_cache
+import heapq
+import itertools
+import math
+import random
+import sys
+
+
+def main(lines: list) -> None:
+    root = deque(lines)
+    CHUNK_SIZE = 25
+    preamble = deque(lines[:CHUNK_SIZE])
+    lines = deque(lines[CHUNK_SIZE:])
+    while True:
+        ans1 = lines.popleft()
+        for comb in itertools.combinations(preamble, 2):
+            x, y = comb
+            s = int(x) + int(y)
+            if s == ans1:
+                preamble.popleft()
+                preamble.append(ans1)
+                break
+        if s != ans1:
+            break
+
+    while True:
+        window_sum = 0
+        items = []
+        for i in range(len(root)):
+            items.append(root[i])
+            window_sum = sum(items)
+            if window_sum > ans1:
+                items.clear()
+                root.popleft()
+                break
+            if window_sum == ans1:
+                break
+
+        if window_sum == ans1:
+            items = sorted(items)
+            ans2 = items[0] + items[-1]
+            break
+
+    print(ans1, ans2)
+
+
+if __name__ == "__main__":
+
+    fin = open("data.in", "r")
+    lines = [int(line.strip()) for line in fin.readlines() if line.strip()]
+    main(lines)
